c0736e8ed496096bd2adec4d78e63245372b2748
pkck28 <kcpkumar33@gmail.com>
2020-05-10 19:17:36 +0530

    Lift Data Added

--- Lift_Drag.m
+++ Lift_Drag.m
@@ -41,9 +41,10 @@ Re_chord_cruise = rho * chord * 0.3048 * V / d_visc;
 
 % ----------------------------------------------------------------------------
 %%% Lift Curve Slope
+% Aircraft.Performance.M_cruise = 0.2;
 
-beta = sqrt(1 - Aircraft.Performance.M_cruise^2);
-k = beta * 7.165/2/pi; % 6.875 from
+beta = sqrt(1 - 0.2^2);
+k = 7.165/2/pi; % 7.165 from XFLR5
 
 F = 1.07 * (1 + Aircraft.Fuselage.diameter/Aircraft.Wing.b)^2;
 
@@ -53,29 +54,23 @@ Aircraft.Wing.Sweep_maxtc = atan( tan(d2r*Aircraft.Wing.Sweep_LE) ...   % Sweepb
                             - ( 4*0.3744*(1 - Aircraft.Wing.taper_ratio) ) ...
                             / ( Aircraft.Wing.Aspect_Ratio*(1 + Aircraft.Wing.taper_ratio) ) )/d2r;
                 
-Aircraft.Wing.lift_slope = 2*pi*Aircraft.Wing.Aspect_Ratio ...
+Aircraft.Lift.lift_slope_M02 = 2*pi*Aircraft.Wing.Aspect_Ratio ...
                     / ( 2 + ( (Aircraft.Wing.Aspect_Ratio*beta/k)^2 ...
                     * ( 1 + ( tan(d2r*Aircraft.Wing.Sweep_maxtc)/beta)^2 ) + 4 )^0.5 );       
                 
-Aircraft.Wing.lift_slope = Aircraft.Wing.lift_slope * S_exposed * F / Aircraft.Wing.S;    % Slope of the lift curve. Raymer Pg. No. 400
+Aircraft.Lift.lift_slope_M02 = Aircraft.Lift.lift_slope_M02 * 0.98; % S_exposed * F / Aircraft.Wing.S;    % Slope of the lift curve. Raymer Pg. No. 400
 
 % ------------------------------------------------------------------------------------
-%%% Maximum CL at Take off condition
-
-% Aircraft.Wing.CL_max_clean = 0.9 * Aircraft.airfoil.cl_max * cos(d2r*Aircraft.Wing.Sweep_qc); % CL max of the wing
-
-% cl_max = 0.47;
-
-% CL = cl_max * (0.952 - 0.45*(Aircraft.Wing.taper_ratio - 0.5)^2 )*( Aircraft.Wing.Aspect_Ratio/12 )^0.03;
+%%% Maximum CL without Flaps or slats
 
 % Methodology taken from Phillips and Alley (2007)
 
-cl_max = 2.6 * ( cos(d2r * Aircraft.Wing.Sweep_qc) ) ;
+cl_max = 2.6 * ( cos(d2r * Aircraft.Wing.Sweep_qc) ) ;  % Reduced for sweepeffects
 
 CL_by_clmax_zero_sweep_zero_twist = (0.952 - 0.45*(Aircraft.Wing.taper_ratio - 0.5)^2 ) ...
                                     *( Aircraft.Wing.Aspect_Ratio/12 )^0.03;    % Ratio of CL/cl_max without twist and sweep
 
-Factor = Aircraft.Wing.lift_slope * Aircraft.Wing.twist * d2r / cl_max;
+Factor = Aircraft.Lift.lift_slope_M02 * Aircraft.Wing.twist * d2r / cl_max;
                                                     
 K1 = 0.15 + 18.5 * (Aircraft.Wing.taper_ratio - 0.4) /  Aircraft.Wing.Aspect_Ratio; % Sweep Correction Coefficient 1
 K2 = 0.55 + 12 * (Aircraft.Wing.taper_ratio - 0.275) /  Aircraft.Wing.Aspect_Ratio;   % Sweep Correction Coefficient 3
@@ -86,12 +81,25 @@ K_sweep = 1 + K1 * d2r * Aircraft.Wing.Sweep_qc ...
 K_stall = 1 + (0.0042 * Aircraft.Wing.Aspect_Ratio - 0.068) ...
          * (1 + 2.3 * Factor ); % Stalling Correction Factor     
      
-K_twist = -0.33; % Calculated from graphs based on the value of "Factor"     
+K_twist = -0.28; % Calculated from graphs based on the value of "Factor" 
 
-% Aircraft.Wing.CL_max = CL_by_clmax_zero_sweep_zero_twist * K_stall ...
-%                       * K_sweep * cl_max * ( 1 - Factor * K_twist );  
+Aircraft.Lift.CL_max_M02 = CL_by_clmax_zero_sweep_zero_twist * K_sweep ...
+  * ( cl_max - K_twist * Aircraft.Lift.lift_slope_M02 * Aircraft.Wing.twist * d2r );  
 
-Aircraft.Wing.CL_max = CL_by_clmax_zero_sweep_zero_twist * K_sweep ...
-  * ( cl_max - K_twist * Aircraft.Wing.lift_slope * Aircraft.Wing.twist * d2r );  
-                  
-%save('Aircraft');
+%% Other Lift Parameters 
+
+Aircraft.Lift.CL_max_M082 = 1.229;
+
+Aircraft.Lift.zero_lift_without_flaps_M082 = -3.6;   % in deg (from Roskam Part 6, Chapeter 8)
+
+Aircraft.Lift.zero_lift_without_flaps_M02 = -4.36;  % in deg (from Roskam Part 6, Chapeter 8)
+
+Aircraft.Lift.lift_slope_M02_with_flaps = 5.636;    % 1/rad (from Roskam Part 6, Chpater 8)
+
+Aircraft.Lift.del_CL_zero_alpha = 0.41; % From Part 6, Chapter 8 
+
+Aircraft.Lift.CL_max_TO = Aircraft.Lift.CL_max + Aircraft.HLD.del_CL_max_flaps_TO + Aircraft.HLD.del_CL_max_slats;
+
+Aircraft.Lift.CL_max_L = Aircraft.Lift.CL_max + Aircraft.HLD.del_CL_max_flaps_L + Aircraft.HLD.del_CL_max_slats;
+
+
