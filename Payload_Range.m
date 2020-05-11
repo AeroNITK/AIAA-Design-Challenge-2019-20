6a7dd4efb6f3dd2c7a001eadef6c3e6d209aa3a5
pkck28 <kcpkumar33@gmail.com>
2020-05-11 15:42:27 +0530

    Payload Range Updated

--- Payload_Range.m
+++ Payload_Range.m
@@ -7,7 +7,7 @@ close all
 
 load('Aircraft.mat');
 
-Max_avail_Fuel = Aircraft.Wing.fuel_volume*7.48052; % Converting from cubic ft to US gallons
+Max_avail_Fuel = 3850*7.48052; % Converting from cubic ft to US gallons
 Max_avail_Fuel = Max_avail_Fuel*6.71;   % Converting from US gallons to lbs
 
 Opt_Weight = Aircraft.Weight.empty_Weight + Aircraft.Weight.crew + ...
@@ -64,7 +64,7 @@ W11byW10 = exp(-(Aircraft.Performance.loiter2*Cj_loiter)/(Aircraft.Aero.LbyD_max
 
 W12byW11 = 0.995;   % Mission Segement Weight Fraction for  Landing (Raymer)
 
-W5byW4 = fuel_fraction/(W1byW_TO*W2byW1*W3byW2*W4byW3*W6byW5  ...
+W5byW4 = weight_fraction/(W1byW_TO*W2byW1*W3byW2*W4byW3*W6byW5  ...
                *W7byW6*W8byW7*W9byW8*W10byW9*W11byW10*W12byW11);
 
 [~,~,~,a] = ISA(Aircraft.Performance.altitude_cruise1*0.3048);
@@ -72,57 +72,36 @@ V = Aircraft.Performance.M_cruise*a;    % Cruising Speed in m/s
            
 range = (V/Cj_cruise)*(Aircraft.Aero.LbyD_max_cruise)*log(W5byW4^-1);
 
-plot(payload_weight,range/1852,'rx','MarkerSize',10);
+range = range/1852;
 
-% Wtomax=454971;
-% w1byWtomax=0.99;
-% w2byw1=0.99;
-% w3byw2=0.995;
-% w4byw3=0.980;
-% w5byw4 = 0.786;
-% w6byw5 = 0.983;
-% w7byw6 = 0.990;
-% w8byw7 = 0.98;
-% w9byw8 = 0.987;
-% w10byw9 = 0.99;
-% w11byw10 = 0.988;
-% w12byw11 = 0.992;
-% 
-% V=237,g=9.81,Cj=0.000139;
-% LbyD=13.67;
-% 
-% Wplmax=94300;Wfmax=4170.84;Wtfo=0.06*Wfmax;Wcrew=1840;We=197830;
-% Woe=We+Wtfo+Wcrew;
-% 
+plot(range,payload_weight,'rx','MarkerSize',10);    % Point C
 
-% 
-% w4byw5=1/(w5byw4);
-% R=(V/(g*Cj))*(LbyD)*log(w4byw5);
-% 
-% %point B
-% Wf=Wtomax-Woe-Wplmax;
-% Mff=1-(Wf/Wtomax);
-% w5byw4 = Mff/((w1byWtomax)*(w2byw1)*(w3byw2)*(w4byw3)*(w6byw5)*(w7byw6)*(w8byw7)*(w9byw8)*(w10byw9)*(w11byw10)*(w12byw11));
-% 
-% plot(R,Wplmax,'rx','MarkerSize',10);
-% 
-% %point C
-% Wpl=Wtomax-Woe-Wfmax;
-% Mff=1-(Wfmax/Wtomax);
-% w5byw4 = Mff/((w1byWtomax)*(w2byw1)*(w3byw2)*(w4byw3)*(w6byw5)*(w7byw6)*(w8byw7)*(w9byw8)*(w10byw9)*(w11byw10)*(w12byw11));
-% 
-% plot(R,Wpl,'rx','MarkerSize',10);
-% 
-% %point D
-% Wto=Woe+Wfmax;
-% Mff=1-(Wfmax/Wto);
-% w5byw4 = Mff/((w1byWtomax)*(w2byw1)*(w3byw2)*(w4byw3)*(w6byw5)*(w7byw6)*(w8byw7)*(w9byw8)*(w10byw9)*(w11byw10)*(w12byw11));
-% 
-% plot(R,Wpl,'rx','MarkerSize',10);
-% 
-% grid on;
-% xlabel('Range (m)');
-% ylabel('Payload (lbs)');
-% title('Payload-Range Diagram');
+% Line between B and C
+x = [Aircraft.Performance.range1,range];
+y = [Aircraft.Weight.payload,payload_weight];
 
+plot(x,y);
 
+% Point D
+
+MTOW = Opt_Weight + Max_avail_Fuel;
+
+fuel_fraction = Max_avail_Fuel/MTOW;
+weight_fraction = 1 - fuel_fraction;
+
+W5byW4 = weight_fraction/(W1byW_TO*W2byW1*W3byW2*W4byW3*W6byW5  ...
+               *W7byW6*W8byW7*W9byW8*W10byW9*W11byW10*W12byW11);
+           
+range1 = (V/Cj_cruise)*(Aircraft.Aero.LbyD_max_cruise)*log(W5byW4^-1);
+
+range1 = range1/1852;
+
+plot(range1,0,'rx','MarkerSize',10);    % Point D    
+
+% Line between C and D
+x = [range,range1];
+y = [payload_weight,0];
+
+plot(x,y);
+
+hold off
