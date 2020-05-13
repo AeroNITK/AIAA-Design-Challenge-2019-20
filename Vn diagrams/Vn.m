07f282b487702c062ce0b818ffc444d7154d56be
TarunHegde <thegde.2000@gmail.com>
2020-05-14 01:01:51 +0530

    Updated Vn codes

--- Vn diagrams/Vn.m
+++ Vn diagrams/Vn.m
@@ -49,17 +49,22 @@ xf=Vd*ones(size(yh))
 ax=gca
 hold on
 plot(x1,y1,'k',x2,y2,'k',x3,y3,'k',x4,y4,'k',x5,y5,'k',x6,y6,'k')
-plot(xa,ya,'k',xa,yb,'--',xa,yc,'--',xb,yd,'--',xc,ye,'--',xd,yf,'--',xe,yg,'--',xf,yh,'--')
+plot(xa,ya,'k',xa,yb,'--k',xa,yc,'--k',xb,yd,'--k',xc,ye,'--k',xd,yf,'--k',xe,yg,'--k',xf,yh,'--k')
+set(findall(gca, 'Type', 'Line'),'LineWidth',1.5);
 hold off
 xlim([0 800])
-ax.XTick=[0 100 200 Vs1 300 375 Va Vc 550 Vd 800]
+ax.XTick=[0 100 200 300 400 500 600 700 800]
 ax.YTick=[-3.0 -2.0 -1.0 0 1.0 2.0 2.5 3.0]
+ax.FontSize = 13
 ylim([-3 5])
 title('V-n Diagram')
 xlabel('Speed V in KEAS')
 ylabel('Load Factor n')
-str={'Vs1','Va','Vc','Vd','n lim pos'}
-text([Vs1 Va Vc Vd 0],[0 0 0 0 2.6],str)
+str={'V_{s1}','V_{a}','V_{c}','V_{d}','n_{lim pos}'}
+text([Vs1 Va Vc Vd 0],[0.25 0.25 0.25 0.25 2.75],str,'FontSize',16)
+dim = [.8 .55 .3 .3];
+str = {'V_{s1} = 267.39','V_{a} = 422.78','V_{c} = 484.69','V_{d} = 605.86'}
+annotation('textbox',dim,'String',str,'FitBoxToText','on')
 
 
 
